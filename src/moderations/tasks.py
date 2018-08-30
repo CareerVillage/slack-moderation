f0b618d5a17476ba8c71f6f61f4b54e3c63187fb
Savio A <savio.abuga@andela.com>
2018-08-30 16:46:57 +0300

    #4 remove prints and add newline

--- src/moderations/tasks.py
+++ src/moderations/tasks.py
@@ -5,10 +5,8 @@ from .models import Moderation
 @background(schedule=10)
 def post_moderation_async(moderation_id, data):
     slack = SlackSdk()
-    print(moderation_id)
     response_data = slack.post_moderation(
         text=data['content'])
-    print(response_data)
     message_id = response_data.get('ts')
     moderation = Moderation.objects.get(id=moderation_id)
     moderation.message_id = message_id
