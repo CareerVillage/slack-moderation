7a97f63cfdb7dbb69145f5e8f299d48ba8af2ffb
Matias Caporale <matiascaporale9@gmail.com>
2022-12-07 11:00:58 -0300

    update libraries

--- src/moderations/serializers.py
+++ src/moderations/serializers.py
@@ -7,8 +7,8 @@ class ModerationSerializer(serializers.ModelSerializer):
     content_key = serializers.CharField(write_only=True)
     content = serializers.CharField(write_only=True)
     content_author_id = serializers.CharField(write_only=True)
-    auto_approve = serializers.NullBooleanField(default=False)
-    auto_flag = serializers.NullBooleanField(default=False)
+    auto_approve = serializers.BooleanField(default=False)
+    auto_flag = serializers.BooleanField(default=False)
 
     class Meta:
         model = ModerationAction
